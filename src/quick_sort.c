47845c77b8282ca2a28f6d7174add5588d41afef
rie maskawa <riemaskawa@rienoMacBook-Air.local>
2018-07-02 23:28:50 +0900

    quicksort

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -9,6 +9,23 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+int i,j,pivot;
+if(n==0) return;
+pivot=A[0];
+for(i=j=1; i<n; i++){
+if(A[i]<=pivot){
+int z=A[j];
+A[j]=A[i];
+A[i]=z;
+j++;
+}
+}
+int w=A[0];
+A[0]=A[j-1];
+A[j-1]=w;
+
+quick_sort(A,j-1);
+quick_sort(A+j,n-j);
 }
 
 int main(){
@@ -22,5 +39,5 @@ int main(){
   quick_sort(A, N);
   for(i=0;i<N;i++){
     if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
-  }
+    }
 }
