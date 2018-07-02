0a3689ad1a84c59a09b75e5404a22d3d152bb445
rie maskawa <riemaskawa@rienoMacBook-Air.local>
2018-07-02 23:51:00 +0900

    quicksort_improved

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -9,7 +9,32 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+int i,l,r,pivot;
+if(n==0) return;
+pivot=A[0];
+for(i=l=1,r=n;i<r;i++){
+if(A[i]<pivot){
+int w=A[l];
+A[l]=A[i];
+A[i]=w;
+l++;
 }
+else if (A[i]>pivot){
+r--;
+int z=A[r];
+A[r]=A[i];
+A[i]=z;
+i--;
+}
+}
+int u=A[l-1];
+A[l-1]=A[0];
+A[0]=u;
+
+quick_sort(A,l-1);
+quick_sort(A+r,n-r);
+}
+
 
 int main(){
   int i;
